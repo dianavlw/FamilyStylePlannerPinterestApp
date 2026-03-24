e404d4a526d9a71ec05a03a10605fce1f55fb40a
Diana Vargas <dianavlw@gmail.com>
2026-03-24 00:00:22 -0700

    modified views and urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from planner_app.views import test_api
+from planner_app.views import test_api, signup, family_members
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/test/', test_api),
+    path("api/signup/", signup),
+    path("api/family-members/", family_members),
 ]
--- backend/planner_app/views.py
+++ backend/planner_app/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_views
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SignupSerializers
