5dd2683904d24aca978854cf0ddb7a16ffa9cfaf
Diana Vargas <dianavlw@gmail.com>
2026-03-23 22:48:54 -0700

    modified the serializers

--- backend/planner_app/serializers.py
+++ backend/planner_app/serializers.py
@@ -11,4 +11,30 @@ class SignupSerializers(serializers.Serializer):
     style_preference = serializers.Charfield(max_lenght=255, required=False, allow_blank=True)
     clothing_size = serializers.Charfield(max_lenght=50, required=False, allow_blank=True)
     favorite_color = serializers.Charfield(max_lenght=50, required=False, allow_blank=True)
-    
+
+    def create(self, validated_data):
+        family_name = validated_data.pop("family_name")
+        role =validated_data.pop("role")
+        style_preferences = validated_data.pop("style_preferences", "")
+        clothing_size = validated_data.pop("clothing_size", "")
+        favorite_color = validated_data.pop("favorite_color", "")
+
+        user = User.objects.create_user(
+            username=validated_data["username"]
+            email=validated_data["email"]
+            password=validated_data["password"],
+            
+        )
+
+        family, _ = Family.objects.get_or_create(name=family_name)
+
+        FamilyMemberProfile.objects.create(
+            user =user,
+            family =family, 
+            role= role, 
+            style_preferences= style_preferences,
+            clothing_size=clothing_size,
+            favorite_color=favorite_color,
+        )
+
+        return user
