e8afd1ac68b9b43f61fa144837b76fc9dae403d5
Diana Vargas <dianavlw@gmail.com>
2026-04-07 22:15:55 -0700

    user signs up and profile is auto generated for them

--- backend/planner_app/apps.py
+++ backend/planner_app/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class PlannerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'planner_app'
+
+    def ready(self):
+        import planner_app.signals
