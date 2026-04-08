b3d2a020ca407c06d98016bccff80154a8f454ba
Diana Vargas <dianavlw@gmail.com>
2026-04-07 21:42:57 -0700

    made new modles for the user profile

--- backend/planner_app/models.py
+++ backend/planner_app/models.py
@@ -17,3 +17,28 @@ class FamilyMemberProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    about_me = models.TextField(blank=True)
+
+    instagram_url = models.URLField(blank=True)
+    pinterest_url = models.URLField(blank=True)
+    tiktok_url = models.URLField(blank=True)
+    other_url = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+    
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boards')
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
